2e133c763956dc47875bec6fbd4ac5253ebed2b2
Wael1231 <106765472+Wael1231@users.noreply.github.com>
2023-06-16 21:09:21 +0100

    Pools objects

--- Il2CppInterop.Runtime/Injection/InjectorHelpers.cs
+++ Il2CppInterop.Runtime/Injection/InjectorHelpers.cs
@@ -67,6 +67,7 @@ namespace Il2CppInterop.Runtime.Injection
         private static readonly Class_GetFieldDefaultValue_Hook GetFieldDefaultValueHook = new();
         private static readonly Class_FromIl2CppType_Hook FromIl2CppTypeHook = new();
         private static readonly Class_FromName_Hook FromNameHook = new();
+        private static readonly GarbageCollector_RunFinalizer_Patch RunFinalizerPatch = new();
 
         internal static void Setup()
         {
